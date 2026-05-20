55c88687c9d8421e91ce0a711aca5a6e30720d6a
xunzhang1128 <162805063+xunzhang1128@users.noreply.github.com>
2026-05-20 16:16:43 +0000

    Deploying to gh-pages from @ xunzhang1128/xunzhang1128.github.io@87286d346cea0a3cf9873e77ccd3fdf1a8415d39 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -481,6 +481,13 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=nhmD_1AAAAAJ", "_blank");
         },
       },{
+        id: 'social-semanticscholar',
+        title: 'Semantic Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.semanticscholar.org/author/2382926154", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
